e147502eecbc7ae09a5bde5425421f3757f294fe
macina <akmacina@yahoo.fr>
2011-03-23 17:33:44 +0100

    ok

---
+++ src/polyrallye/modele/Frottements.java
@@ -0,0 +1,71 @@
+package polyrallye.modele;
+
+public class Frottements {
+    public double resistanceAir;
+    public double boue;
+    public double sable;
+    public double asphalt;
+    public double gravier;
+    public double terre;
+    public double herbe;
+    
+
+    public Frottements(){
+        
+    }
+    
+    /**
+     * @return the resistanceAir
+     */
+    public double getResistanceAir() {
+        return resistanceAir;
+    }
+
+
+    /**
+     * @return the boue
+     */
+    public double getBoue() {
+        return boue;
+    }
+
+
+    /**
+     * @return the sable
+     */
+    public double getSable() {
+        return sable;
+    }
+
+
+    /**
+     * @return the asphalt
+     */
+    public double getAsphalt() {
+        return asphalt;
+    }
+
+
+    /**
+     * @return the gravier
+     */
+    public double getGravier() {
+        return gravier;
+    }
+
+
+    /**
+     * @return the terre
+     */
+    public double getTerre() {
+        return terre;
+    }
+
+
+    /**
+     * @return the herbe
+     */
+    public double getHerbe() {
+        return herbe;
+    }
+}
