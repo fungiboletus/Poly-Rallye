cfac2f9f75045db4ed25f5af6a73bc6ed63eaa13
Antoine Pultier <antoine@blogjaune.fr>
2011-05-19 15:28:01 +0200

    caca

--- VocalyzeSIVOX/bin/execution.bat
+++
@@ -1,8 +0,0 @@
-set PATH=../../lib/;../../jre/bin/
-
-rem attention, pas d'expace derrière le =
-java -Djava.library.path=../ressources/lib -jar SI_VOX.jar
-
-rem utilisation muette pour faire une démonstration
-rem java t2s/Main -f mot.txt tutu 
-rem crée tutu.wav et tutu.pho
